23123bfa015dae883e04b98007ba8f4030d40adf
thegreat1200 <junk_mail000@icloud.com>
2015-08-25 17:53:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 $("body").append("<div id='mobyfun' style='position: fixed; top: 10%; right: 0px; height: 30%; background: lightgray; border-top-left-radius: 10px; border-bottom-left-radius: 10px;'><h1>MobyFun</h1></div>");
+$("body").append("<script>function blueButton() {$('.lab-next').css('background','background:url(/MM/img/lab/MSNextBlue.png) no-repeat 0 0');}</script>");
+$("#mobyfun").append("<button id='blue' onClick="blueButton()">Blue Button</button>");
